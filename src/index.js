207b7e3b194c872bfa61d0cbebc482d3b87a9764
Scott Moss <willscottmoss@gmail.com>
2017-10-16 08:24:28 -0500

    chore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import http from 'http'
-// import { execute, subscribe } from 'graphql'
 import { createServer } from 'http'
 
 import app from './server'
-// import schema from './schema'
-
-const server = http.createServer(app)
 let currentApp = app
 
-server.listen(3000, () => {
-	console.log('Server listening on port 3000')
-})
 
 if (module.hot) {
 	module.hot.accept(['./server'], () => {
--- src/server.js
+++ src/server.js
@@ -4,17 +4,6 @@ import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
-const app = express()
-
-setupMiddware(app)
-connect()
-// setup basic routing for index route
-
-app.use('/signin', signin)
-
-// catch all
-app.all('*', (req, res) => {
-  res.json({ok: true})
-})
+const app = null
 
 export default app
