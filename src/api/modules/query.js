9fde5f2ec3c1c41abf72dc4bef4f01dc3cd943a1
Scott Moss <willscottmoss@gmail.com>
2017-10-16 08:16:51 -0500

    chore

--- src/api/modules/query.js
+++ src/api/modules/query.js
@@ -29,51 +29,27 @@ export const controllers = {
 }
 
 export const createOne = (model) => (req, res, next) => {
-  return controllers.createOne(model, req.body)
-    .then(doc => res.status(201).json(doc))
-    .catch(error => next(error))
+
 }
 
 export const updateOne = (model) => async (req, res, next) => {
-  const docToUpdate = req.docFromId
-  const update = req.body
 
-  return controllers.updateOne(docToUpdate, update)
-    .then(doc => res.status(201).json(doc))
-    .catch(error => next(error))
 }
 
 export const deleteOne = (model) => (req, res, next) => {
-  return controllers.deleteOne(req.docFromId)
-    .then(doc => res.status(201).json(doc))
-    .catch(error => next(error))
+
 }
 
 export const getOne = (model) => (req, res, next) => {
-  return controllers.getOne(req.docToUpdate)
-    .then(doc => res.status(200).json(doc))
-    .catch(error => next(error))
+
 }
 
 export const getAll = (model) => (req, res, next) => {
-  return controllers.getAll(model)
-    .then(docs => res.json(docs))
-    .catch(error => next(error))
+
 }
 
 export const findByParam = (model) => (req, res, next, id) => {
-  return controllers.findByParam(model, id)
-    .then(doc => {
-      if (!doc) {
-        next(new Error('Not Found Error'))
-      } else {
-        req.docFromId
-        next()
-      }
-    })
-    .catch(error => {
-      next(error)
-    })
+  
 }
 
 
