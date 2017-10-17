ff8b145a5f904df7a0649512a7b1fed74c1c814d
Scott Moss <willscottmoss@gmail.com>
2017-10-17 07:56:18 -0500

    chore

--- src/api/resources/playlist/playlist.resolvers.js
+++ src/api/resources/playlist/playlist.resolvers.js
@@ -8,9 +8,24 @@ const allPlaylists = () => {
   return Playlist.find({}).exec()
 }
 
+const newPlaylist = (_, {input}) => {
+  return Playlist.create(input)
+}
+
+const updatePlaylist = (_, {input}) => {
+  const {id, ...update} = input
+
+  return Playlist.findByIdAndUpdate(id, update, {new: true}).exec()
+}
+
 export const playlistResolvers = {
   Query: {
     allPlaylists,
     Playlist: getPlaylist,
+  },
+
+  Mutation: {
+    newPlaylist,
+    updatePlaylist
   }
 }
--- src/api/resources/song/song.resolvers.js
+++ src/api/resources/song/song.resolvers.js
@@ -8,9 +8,24 @@ const allSongs = () => {
   return Song.find({}).exec()
 }
 
+const newSong = (_, {input}) => {
+  return Song.create(input)
+}
+
+const updateSong = (_, {input}) => {
+  const {id, ...update} = input
+
+  return Song.findByIdAndUpdate(id, update, {new: true}).exec()
+}
+
 export const songResolvers = {
   Query: {
     allSongs,
     Song: getSong
+  },
+
+  Mutation: {
+    newSong,
+    updateSong
   }
 }
